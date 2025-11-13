16cd556ce419d1db3b4f701880370b9bf1dd4b5a
Rafi <rafi.annas2000@gmail.com>
2025-11-13 14:49:53 +0700

    add print request

--- tests_explorer/test_case/api_test.js
+++ tests_explorer/test_case/api_test.js
@@ -8,6 +8,7 @@ describe('🧪 API Testing Demo', () => {
     const res = await request(baseConfig.baseURL)
       .get(Endpoints.getAllUsers)
      // .set(createHeaders());
+    console.log("req = ", res.request);
 
     expect(res.status).to.equal(200);
     expect(res.body.total).to.be.equal(12);
