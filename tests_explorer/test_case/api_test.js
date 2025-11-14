f48426fcd4cffe28ab1b6577bd6f0ff73ff84509
Rafi <rafi.annas2000@gmail.com>
2025-11-14 14:17:06 +0700

    add ipv4Agent

--- tests_explorer/test_case/api_test.js
+++ tests_explorer/test_case/api_test.js
@@ -3,15 +3,23 @@ import { expect } from 'chai';
 import { Endpoints } from '../object_repository/endpoints.js';
 import { baseConfig, createHeaders } from '../utils/helper.js';
 
+import https from "https";
+import dns from "dns";
+
 describe('🧪 API Testing Demo', () => {
+
+  const ipv4Agent = new https.Agent({
+  family: 4,
+  lookup: (hostname, opts, cb) => {
+    dns.lookup(hostname, { family: 4 }, cb);
+  }
+});
+
   it('GET All Users', async () => {
     const res = await request(baseConfig.baseURL)
       .get(Endpoints.getAllUsers)
-      .set("User-Agent", "Mozilla/5.0")  
-      .set("Accept", "application/json")
-      .set("Connection", "close");
+      .agent(ipv4Agent)
      // .set(createHeaders());
-    console.log('REQUEST HEADERS:', res.req._headers);
 
     expect(res.status).to.equal(200);
     expect(res.body.total).to.be.equal(12);
@@ -20,6 +28,7 @@ describe('🧪 API Testing Demo', () => {
   it('GET 1 User', async () => {
     const res = await request(baseConfig.baseURL)
       .get(Endpoints.getUsers)
+      .agent(ipv4Agent)
      // .set(createHeaders());
 
     expect(res.status).to.equal(200);
